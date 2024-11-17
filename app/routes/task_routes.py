6206764412d66fc08381d48f0f6c21130de12233
Christelle Nkera <scnkera@gmail.com>
2024-11-17 06:10:32 -0500

    Updated POST, GET, PUT, and DELETE /tasks database requests

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from ..db import db
 from app.models.task import Task
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
 @tasks_bp.post("")
-def create_cat():
+def create_task():
     request_body = request.get_json()
     title = request_body["title"]
     description = request_body["description"]
@@ -19,12 +19,40 @@ def create_cat():
     return response, 201
 
 @tasks_bp.get("")
-def get_all_cats():
+def get_all_tasks():
     query = db.select(Task).order_by(Task.id)
     tasks = db.session.scalars(query)
 
     tasks_response = [task.to_dict() for task in tasks]
     return tasks_response
+
+@tasks_bp.get("/<task_id>")
+def get_single_task(task_id):
+    task = validate_task(task_id)
+
+    return task.to_dict()
+
+@tasks_bp.put("/<task_id>")
+def update_task(task_id):
+    task = validate_task(task_id)
+
+    request_body = request.get_json()
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    task.completed_at = request_body["completed_at"]
+
+    db.session.commit()
+
+    return Response(status=204, mimetype='application/json')
+
+@tasks_bp.delete("/<task_id>")
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return Response(status=204, mimetype='application/json')
 
 def validate_task(task_id):
 
@@ -35,8 +63,10 @@ def validate_task(task_id):
         abort(make_response({"message": f"Task id {task_id} not found"}, 400))
 
     # returns task with the corresponding task_id
-    for task in tasks:
-        if task.id == task_id:
-            return task
-        
-    abort(make_response({"message": f"Task id {task_id} not found"}, 404))
+    query = db.select(Task).where(Task.id == task_id)
+    task = db.session.scalar(query)
+
+    if not task:
+        abort(make_response({"message": f"Task id {task_id} not found"}, 404))
+
+    return task
