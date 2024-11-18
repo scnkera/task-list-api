65fd5511a5e992a73f8120fc643b5e8083f568d1
Christelle Nkera <scnkera@gmail.com>
2024-11-18 11:27:59 -0500

    Uncommented out code

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,5 +1,5 @@
-# from sqlalchemy.orm import Mapped, mapped_column
-# from ..db import db
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
 
-# class Goal(db.Model):
-#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+class Goal(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
