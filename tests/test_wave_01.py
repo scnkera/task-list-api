3f844192b4dd9e4cfb00bb3a1cbb30b00564c7a3
Christelle Nkera <scnkera@gmail.com>
2024-12-06 16:34:05 -0500

    Added test assertions

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -59,8 +59,10 @@ def test_get_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
+    assert response_body == {"message": f"Task id 1 not found"}
 
-    raise Exception("Complete test with assertion about response body")
+
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
@@ -130,8 +132,9 @@ def test_update_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
+    assert response_body == {'message': 'Task id 1 not found'}
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
@@ -160,8 +163,10 @@ def test_delete_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
+    assert response_body == {'message': 'Task id 1 not found'}
 
-    raise Exception("Complete test with assertion about response body")
+
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
