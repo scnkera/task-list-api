f9b850a38b0ec73e08a991fa0d3d4a52079c7995
Christelle Nkera <scnkera@gmail.com>
2024-12-10 12:01:40 -0500

    Updated standard validation and creation models

--- app/routes/route_utilities.py
+++ app/routes/route_utilities.py
@@ -21,51 +21,30 @@ def validate_model(cls, model_id):
 
     return model
 
-# def create_model(cls, model_data):
-#     try:
-#         new_model = cls.from_dict(model_data)
+def create_model(cls, model_data):
+    try:
+        new_model = cls.from_dict(model_data)
         
-#     except KeyError as error:
-#         response = {"details": "Invalid data"}
-#         abort(make_response(response, 400))
+    except KeyError as error:
+        response = {"details": "Invalid data"}
+        abort(make_response(response, 400))
     
-#     db.session.add(new_model)
-#     db.session.commit()
+    db.session.add(new_model)
+    db.session.commit()
 
-#     return new_model.to_dict(), 201
+    return {"task": new_model.to_dict()}, 201
 
-def get_models_filtered(cls, filter_params=None):
+def get_models_with_filters(cls, filters=None):
     query = db.select(cls)
     
-    if filter_params:
-        # Set default order_by attribute
-        order_by = getattr(cls, filter_params.get("order_by", "title"))
-        
-        # Apply sorting based on the "sort" parameter
-        if filter_params.get("sort") == "desc":
-            query = query.order_by(desc(order_by))
-        else:  # Default to ascending order
-            query = query.order_by(asc(order_by))
+    if filters:
+        order_by = getattr(cls, filters.get("order_by", "title"))
+        if filters.get("sort") == "desc":
+            query = query.order_by(db.desc(order_by))
+        else:
+            query = query.order_by(db.asc(order_by))
     
-    return query
+    models = db.session.scalars(query)
+    models_response = [model.to_dict() for model in models]
 
-
-# def validate_model_id(cls, model_id):
-#     # checks for valid input
-#     try:
-#         model_id = int(model_id)
-#     except:
-#         response = {"msg": f"{cls.__name__} id {model_id} is invalid"}
-#         abort(make_response(response, 400))
-
-#     # returns task with the corresponding task_id
-#     query = db.select(cls).where(cls.id == model_id)
-#     model = db.session.scalar(query)
-
-#     if not model:
-#         response = {"msg": f"{cls.__name__} {model_id} not found."}
-#         abort(make_response(response, 404))
-
-#     return model
-
-# route_utilities.py
+    return models_response
