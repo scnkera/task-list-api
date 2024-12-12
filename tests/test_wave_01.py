107267f927a9c06fecbaf42f619badc1655bbf06
Christelle Nkera <scnkera@gmail.com>
2024-12-12 13:44:18 -0500

    Changed response structure

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -59,7 +59,7 @@ def test_get_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-    assert response_body == {"message": f"Task id 1 not found"}
+    assert response_body == {"msg":"Task 1 not found."}
 
 
     # raise Exception("Complete test with assertion about response body")
@@ -132,7 +132,7 @@ def test_update_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-    assert response_body == {'message': 'Task id 1 not found'}
+    assert response_body == {"msg":"Task 1 not found."}
 
     # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
@@ -163,7 +163,7 @@ def test_delete_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-    assert response_body == {'message': 'Task id 1 not found'}
+    assert response_body == {"msg":"Task 1 not found."}
 
 
     # raise Exception("Complete test with assertion about response body")
