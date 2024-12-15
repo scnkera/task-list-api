062a77bbe5c8b4549a854935376db7dc34502291
Christelle Nkera <scnkera@gmail.com>
2024-12-15 03:04:48 -0500

    Refactored code and imported validate_model and create_model

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -5,6 +5,7 @@ import datetime
 import requests
 import os
 from sqlalchemy import desc, asc
+from .route_utilities import validate_model, create_model
 
 goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
 
@@ -12,19 +13,7 @@ goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
 def create_goal():
     request_body = request.get_json()
 
-    try:
-        title = request_body["title"]
-    except KeyError:
-        response = {"details": "Invalid data"}
-        abort(make_response(response, 400))
-
-    new_goal = Goal(title=title)
-
-    db.session.add(new_goal)
-    db.session.commit()
-
-    response = {"goal": new_goal.to_dict()}
-    return response, 201
+    return create_model(Goal, request_body)
 
 @goals_bp.get("")
 def get_all_goals():
@@ -37,44 +26,30 @@ def get_all_goals():
 
 @goals_bp.get("/<goal_id>")
 def get_single_goal(goal_id):
-    goal = validate_goal_id(goal_id)
+    goal = validate_model(Goal, goal_id)
+    response = {"goal": goal.to_dict()}
 
-    return {"goal": goal.to_dict()}, 200
+    return response, 200
 
 @goals_bp.put("/<goal_id>")
 def update_goal(goal_id):
-    goal=validate_goal_id(goal_id)
+    goal = validate_model(Goal, goal_id)
+    
     request_body = request.get_json()
-
     goal.title = request_body["title"]
+    
     db.session.commit()
-
     response = {"title": goal.title}
 
     return response, 200
 
 @goals_bp.delete("/<goal_id>")
 def delete_goal(goal_id):
-    goal = validate_goal_id(goal_id)
-    
+    goal = validate_model(Goal, goal_id)
+
     db.session.delete(goal)
     db.session.commit()
 
     response = {"details": f"Goal {goal.id} \"{goal.title}\" successfully deleted"}
+
     return response, 200
-
-def validate_goal_id(goal_id):
-    try:
-        goal_id = int(goal_id)
-    except:
-        response = {"msg": f"Goal_id {goal_id} is invalid"}
-        abort(make_response(response, 400))
-
-    query = db.select(Goal).where(Goal.id == goal_id)
-    found_goal = db.session.scalar(query)
-
-    if not found_goal:
-        response = {"msg": f"Goal {goal_id} not found."}
-        abort(make_response(response, 404))
-
-    return found_goal
